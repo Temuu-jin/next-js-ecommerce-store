4b8e72eecac0fdaeebe028d3c9a23dba8ff5703a
Temuu-jin <temucin.d.k@gmail.com>
2023-10-12 14:33:13 +0200

    Make Image unoptimized

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,4 +1,4 @@
-// import Image from 'next/image';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import { getProductById } from '../../../database/products';
 import { getParsedCart } from '../../../util/cookies';
@@ -45,12 +45,13 @@ export default async function ProductPage(props: ProductPageProps) {
       <div className={styles.card}>
         <h1>{singleProduct.name}</h1>
         <p>{singleProduct.description}</p>
-        <img
+        <Image
           src={singleProduct.image}
           alt={singleProduct.name}
           width={200}
           height={200}
           data-test-id="product-image"
+          unoptimized
         />
         €<p data-test-id="product-price">{singleProduct.price}</p>
       </div>

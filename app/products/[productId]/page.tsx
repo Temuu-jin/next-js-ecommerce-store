2bcbaf79489e316703b785c231275bc60a90253a
Temuu-jin <temucin.d.k@gmail.com>
2023-10-12 16:23:35 +0200

    Playwright Lint Error Fix

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import { getProductById } from '../../../database/products';
 import { getParsedCart } from '../../../util/cookies';
